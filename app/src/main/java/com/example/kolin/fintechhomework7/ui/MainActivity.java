20d6f58eb2a8e0385c987776567f1f531fa3f6c4
KolinLoures <kolin-1994@mail.ru>
2017-11-12 20:32:37 +0300

    move main activity to package ui

--- app/src/main/java/com/example/kolin/fintechhomework7/ui/MainActivity.java
+++ app/src/main/java/com/example/kolin/fintechhomework7/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.kolin.fintechhomework7;
+package com.example.kolin.fintechhomework7.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.kolin.fintechhomework7.R;
 
 public class MainActivity extends AppCompatActivity {
 
